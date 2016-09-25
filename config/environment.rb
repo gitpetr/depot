b4b56577b81b376d24013c5682fe1b71b50801b1
gitpetr <pkhilenko@yandex.ru>
2016-09-25 17:36:37 +0700

    complete order

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:    "smtp.gmail.com",
+    port:    587,
+    domain:    "domain.of.sender.net",
+    authentication:    "plain",
+    user_name:    "pkhilenko",
+    password:    "c_1965-tatinu1965",
+    enable_starttls_auto: true
+}
+end
